3f58410fd82cf25f5114360427fb76ba50573fe8
rhy511 <samylh1994511@gmail.com>
2020-05-03 17:02:59 +0800

    driver.close()

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 driver = webdriver.Chrome()
 driver.get('http://www.youtube.com')
-driver.close()
+#driver.close()
